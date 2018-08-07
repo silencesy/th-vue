07b5c86c142cdef9a76c5fda7b805ea072c9c77f
R <6106786451@qq.com>
2018-08-07 16:08:18 +0800

    123

--- src/components/Pages/Address/AddAddress.vue
+++ src/components/Pages/Address/AddAddress.vue
@@ -17,19 +17,19 @@
 					</div>
 					<div>
 						<label for="add"><i>*</i> 选择地址：</label> 
-						<span>{{myAddressProvince}} {{myAddressCity}} <!-- {{myAddresscounty}} --></span>
+						<span>{{myAddressProvince}} {{myAddressCity}}</span>
 					</div>
 					<div>
 		        <mt-picker :slots="myAddressSlots" @change="onMyAddressChange"></mt-picker>
 		      </div>
 				</div>
 				<div class="address">
-					<textarea rows="5" v-model='address' placeholder="* Please write down your detailed address in Chinese(city name is required)"> </textarea>
-					<textarea rows="5" placeholder="Please write down your detailed address in English(city name is required)"> </textarea>
+					<textarea rows="5" v-model='address' placeholder="* Please write down your detailed address in Chinese."> </textarea>
+					<!-- <textarea rows="5" placeholder="Please write down your detailed address in English(city name is required)"> </textarea> -->
 				</div>
 				<div class="addDefault">
-					<input class="defaultRadio" type="radio" id="item" value="item">
-	       	<label for="item">Default</label>
+					<input class="defaultCheckbox" v-model="defaultBtn" type="checkbox" id="item">
+	       			<label for="item">Default</label>
 				</div>
 			</div>
 			<div class="bottom">Submit</div>
@@ -38,65 +38,56 @@
 </template>
 <script>
 	import { Picker } from 'mint-ui';
-  import myaddress from './address.json';
-	// import { Toast } from 'mint-ui';
+  	import myaddress from './address.json';
 	export default {
 		name: 'AddAdress',
 		components: {
-      'mt-picker': Picker
-    },
+      		'mt-picker': Picker
+    	},
 		data () {
 			return {
-       	myAddressSlots: [
-          {
-            flex: 1,
-            defaultIndex: 1,    
-            values: Object.keys(myaddress),  //省份数组
-            className: 'slot1',
-            textAlign: 'center'
-          }, {
-            divider: true,
-            content: '-',
-            className: 'slot2'
-          }, {
-            flex: 1,
-            values: [],
-            className: 'slot3',
-            textAlign: 'center'
-          },
-          {
-            divider: true,
-            content: '-',
-            className: 'slot4'
-          },
-          {
-            flex: 1,
-            values: [],
-            className: 'slot5',
-            textAlign: 'center'
-          }
-        ],
-        myAddressProvince:'省',
-        myAddressCity:'市',
-        myAddresscounty:'区/县',
+				name: '',
+				number: null,
+				email: '',
+				address: '',
+				defaultBtn: false,
+		       	myAddressSlots: [
+					{
+						flex: 1,
+						defaultIndex: 1,    
+						values: Object.keys(myaddress),  //省份数组
+						className: 'slot1',
+						textAlign: 'center'
+					}, {
+						divider: true,
+						content: '-',
+						className: 'slot2'
+					}, {
+						flex: 1,
+						values: [],
+						className: 'slot3',
+						textAlign: 'center'
+					}
+		        ],
+		        myAddressProvince:'省',
+		        myAddressCity:'市',
 			}
 		},
 		mounted () {
 			this.$nextTick(() => {
-        this.myAddressSlots[0].defaultIndex = 0    
-      });
+        		this.myAddressSlots[0].defaultIndex = 0    
+      		});
 		},
 		methods: {
 			onMyAddressChange(picker, values) {
-       if(myaddress[values[0]]){  //这个判断类似于v-if的效果（可以不加，但是vue会报错，很不爽）
-          picker.setSlotValues(1,Object.keys(myaddress[values[0]])); // Object.keys()会返回一个数组，当前省的数组
-          picker.setSlotValues(2,myaddress[values[0]][values[1]]); // 区/县数据就是一个数组
-          this.myAddressProvince = values[0];
-          this.myAddressCity = values[1];
-          this.myAddresscounty = values[2];
-        }
-      },
-
+		       if(myaddress[values[0]]){  //这个判断类似于v-if的效果（可以不加，但是vue会报错，很不爽）
+		          picker.setSlotValues(1,Object.keys(myaddress[values[0]])); // Object.keys()会返回一个数组，当前省的数组
+		          // picker.setSlotValues(2,myaddress[values[0]][values[1]]); // 区/县数据就是一个数组
+		          this.myAddressProvince = values[0];
+		          this.myAddressCity = values[1];
+		          // this.myAddresscounty = values[2];
+		        }
+      		}
 		}
 	}
 </script>
@@ -109,9 +100,9 @@
 		margin-bottom: 56px;
 	}
 	.info input {
-    border: none;
-    outline: none;
-    color: #999;
+		border: none;
+		outline: none;
+		color: #999;
 	}
 	.baseInfo > div {
 		padding: 10px;
@@ -151,8 +142,8 @@
 	}
 	.baseInfo span {
 		display: inline-block;
-    line-height: 40px;
-    color: #999;
+		line-height: 40px;
+		color: #999;
 	}
 	.address {
 		margin: 0 10px;
@@ -161,85 +152,34 @@
 		margin-top: 20px;
 	}
 	textarea {
-    border: 1px solid #dfdfdf;
-    width: 100%;
-    border-radius: 4px;
-    padding: 10px;
-    box-sizing: border-box;
-    margin-bottom: 15px;
-    font: inherit;
-    color: #999;
-    line-height: 24px;
-    outline: none;
-    resize: none;
+		border: 1px solid #dfdfdf;
+		width: 100%;
+		border-radius: 4px;
+		padding: 10px;
+		box-sizing: border-box;
+		margin-bottom: 15px;
+		font: inherit;
+		color: #999;
+		line-height: 24px;
+		outline: none;
+		resize: none;
 	}
-	.defaultRadio {
-	  position: absolute;
-	  display: none;
-	}
-  .defaultRadio + label {
-    position: relative;
-    display: block;
-    padding-left: 30px;
-    cursor: pointer;
-    vertical-align: middle;
-  }
-  .defaultRadio + label:hover:before {
-    animation-duration: 0.4s;
-    animation-fill-mode: both;
-    animation-name: hover-color;
-  }
-  .defaultRadio + label:before {
-    position: absolute;
-    top: 0px;
-    left: 0px;
-    display: inline-block;
-    width: 19px;
-    height: 19px;
-    content: '';
-    border: 1px solid #c0c0c0;
-  }
-  .defaultRadio + label:after {
-    position: absolute;
-   	display: none;
-    content: '';
-  }
-  .defaultRadio:checked + label:before {
-    animation-name: none;
-  }
-  .defaultRadio:checked + label:after {
-    display: block;
-  }
-  .defaultRadio + label:before {
-    border-radius: 50%;
-  }
-  .defaultRadio + label:after {
-    top: 7px;
-    left: 7px;
-    width: 7px;
-    height: 7px;
-    border-radius: 50%;
-    background: #f9421e;
-  }
-  .defaultRadio:checked + label:before {
-    border: 1px solid #f9421e;
-  }
 	.addDefault {
 		margin-left: 10px;
 		padding-bottom: 20px;
 	}
 	.bottom {
 		position: fixed;
-    display: block;
-    height: 36px;
-    line-height: 36px;
-    background: #F9421E;
-    bottom: 0;
-    left: 0;
-    right: 0;
-    margin: 0 10px 10px 10px;
-    color: #fff;
-    text-align: center;
-    border-radius: 23px;
+	    display: block;
+	    height: 36px;
+	    line-height: 36px;
+	    background: #F9421E;
+	    bottom: 0;
+	    left: 0;
+	    right: 0;
+	    margin: 0 10px 10px 10px;
+	    color: #fff;
+	    text-align: center;
+	    border-radius: 23px;
 	}
 </style>
